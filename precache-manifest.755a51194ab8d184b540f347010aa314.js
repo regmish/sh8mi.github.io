e4f7732d1e01dba545fa0ff7234554ea65ff4252
shankarregmi <shankarregmi@gmail.com>
2019-05-18 21:59:52 +0200

    try react build

---
+++ precache-manifest.755a51194ab8d184b540f347010aa314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db51c9b12d2a981376f3",
+    "url": "/static/js/main.db51c9b1.chunk.js"
+  },
+  {
+    "revision": "2775f0869aa58847e136",
+    "url": "/static/js/1.2775f086.chunk.js"
+  },
+  {
+    "revision": "db51c9b12d2a981376f3",
+    "url": "/static/css/main.72ed6df9.chunk.css"
+  },
+  {
+    "revision": "2775f0869aa58847e136",
+    "url": "/static/css/1.bfc0e887.chunk.css"
+  },
+  {
+    "revision": "d08a2892a691da256a2a0791bf9fbd23",
+    "url": "/index.html"
+  }
+];
